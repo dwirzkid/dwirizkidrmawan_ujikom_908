25a5d4071aedc98304bed5f54e31924276867eef
dwirzkid <124743771+dwirzkid@users.noreply.github.com>
2025-03-16 12:35:38 +0700

    feat: ujikom test 7

---
+++ ujikom-php-2/view/home_view.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Bootstrap demo</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+        crossorigin="anonymous" />
+</head>
+
+<body>
+    <content>
+        <div class="mt-4 px-2 me-5">
+            <p class="text-start fw-bold fs-1">Tambah Data Barang</p>
+
+            <div class="row mb-3">
+
+
+                <div class="mt-3 row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Nasi Goreng</label>
+
+
+
+                    <div class="col">
+                        <input type="text" class="form-control" id="staticEmail" />
+                    </div>
+                </div>
+                <div class="mt-3 row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Ayam Goreng</label>
+
+
+
+                    <div class="col">
+                        <input type="text" class="form-control" id="staticEmail" />
+                    </div>
+                </div>
+                <div class="mt-3 row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Kopi</label>
+
+
+
+                    <div class="col">
+                        <input type="text" class="form-control" id="staticEmail" />
+                    </div>
+                </div>
+            </div>
+
+
+
+            <button type="button" class="btn btn-primary mt-5">Hitung Total</button>
+        </div>
+    </content>
+
+    <div class="mt-5 px-2">
+        <footer class="blockquote-footer">
+            Total Pesanan <cite title="Source Title"> Rp.70.000</cite>
+        </footer>
+    </div>
+
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
